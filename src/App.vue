a729f1c5ea972ed769fc95ce5cf687057b8d75a9
Tom Metcalfe <iamtommetcalfe@gmail.com>
2023-09-23 14:13:34 +0100

    corrected home link to factor in for Github pages sub directories

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
     <main>
       <header class="py-3 mb-4 border-bottom">
         <div class="container d-flex flex-wrap justify-content-center">
-          <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
-          <span class="fs-4">
-            <img :src="pokemonLogo" alt="Pokemon Logo" class="d-inline-block align-top" width="120" height="67">
-          </span>
-          </a>
+          <router-link to="/" href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
+            <span class="fs-4">
+              <img :src="pokemonLogo" alt="Pokemon Logo" class="d-inline-block align-top" width="120" height="67">
+            </span>
+          </router-link>
           <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
             <input type="search" v-model="searchQuery" class="form-control" placeholder="Search..." aria-label="Search">
           </form>
