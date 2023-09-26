734d4f0085c82c3c739b2a225166f5b645ef5b7f
Tom Metcalfe <iamtommetcalfe@gmail.com>
2023-09-26 22:06:32 +0100

    updated manifest config

--- vue.config.js
+++ vue.config.js
@@ -8,13 +8,69 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: basePath,
   pwa: {
-    name: 'My Vue Pokedex',
+    name: 'My Vue Pokédex',
     themeColor: '#2a75bb',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
     manifestOptions: {
-      start_url: basePath
+      start_url: basePath,
+      "icons": [
+        {
+          "src": "img/icons/icon-48x48.png",
+          "sizes": "48x48",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "img/icons/icon-72x72.png",
+          "sizes": "72x72",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "img/icons/icon-96x96.png",
+          "sizes": "96x96",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "img/icons/icon-128x128.png",
+          "sizes": "128x128",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "img/icons/icon-144x144.png",
+          "sizes": "144x144",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "img/icons/icon-152x152.png",
+          "sizes": "152x152",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "img/icons/icon-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "img/icons/icon-384x384.png",
+          "sizes": "384x384",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "img/icons/icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable any"
+        }
+      ]
     },
 
     // Workbox options
