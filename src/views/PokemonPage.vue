c8d503177ad835683c486fb2902c2042d5a58fd7
Tom Metcalfe <iamtommetcalfe@gmail.com>
2023-09-26 20:41:04 +0100

    added some more detail to the model ... and installed the PWA Vue CLI

--- src/views/PokemonPage.vue
+++ src/views/PokemonPage.vue
@@ -5,10 +5,24 @@
     </div>
 
     <div v-else-if="pokemonDetails">
-      <h1>{{ capitalize(pokemonDetails.name) }}</h1>
-      <img :src="pokemonDetails.sprites.front_default" alt="Pokemon image" v-if="pokemonDetails.sprites && pokemonDetails.sprites.front_default">
-      <p>Type: {{ pokemonDetails.types[0].type.name }}</p>
-      <!-- Add more details as desired. The above are just examples and may need modification based on the API's response structure. -->
+      <div class="pokemon-card-header">
+        <h1>{{ capitalize(pokemonDetails.name) }}</h1>
+      </div>
+      <div class="pokemon-card-image">
+        <img :src="pokemonDetails.sprites.front_default" alt="Pokemon image" v-if="pokemonDetails.sprites && pokemonDetails.sprites.front_default">
+      </div>
+      <div class="pokemon-card-details">
+        <p v-if="pokemonDetails.types && pokemonDetails.types.length > 0"><b>Type: </b>
+          <span v-for="(typeObj, index) in pokemonDetails.types" :key="index">
+          {{ capitalize(typeObj.type.name) }}<span v-if="index < pokemonDetails.types.length - 1">, </span>
+      </span>
+        </p>
+        <p v-if="pokemonDetails.stats && pokemonDetails.stats.length > 0"><b>Stats: </b>
+          <span v-for="(statObj, index) in pokemonDetails.stats" :key="index">
+        {{ capitalize(statObj.stat.name) }} (Base: {{ statObj.base_stat }}, Effort: {{ statObj.effort }})<span v-if="index < pokemonDetails.stats.length - 1">, </span>
+      </span>
+        </p>
+      </div>
     </div>
 
     <div v-else-if="errorMessage">
@@ -18,24 +32,30 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref, PropType } from 'vue';
+import { defineComponent, ref, onMounted, PropType } from 'vue';
 import { getPokemonByName } from '@/api/pokemonAPI';
 import { capitalize } from '@/helpers';
+
+interface PokemonDetails {
+  name: string;
+  types: Array<{ type: { name: string } }>;
+  stats: Array<{ stat: { name: string }, base_stat: number, effort: number }>;
+  sprites: { front_default: string };
+}
 
 export default defineComponent({
   props: {
     name: String as PropType<string>,
   },
   setup(props) {
-    const pokemonDetails = ref(null);
+    const pokemonDetails = ref<PokemonDetails | null>(null);
     const loading = ref(true);
     const errorMessage = ref<string | null>(null);
 
     onMounted(async () => {
       if (props.name) {
         try {
-          const response = await getPokemonByName(props.name);
-          pokemonDetails.value = response;
+          pokemonDetails.value = await getPokemonByName(props.name);
         } catch (error) {
           console.error("Failed to fetch Pokémon details:", error);
           errorMessage.value = "Failed to load Pokémon details. Please try again later.";
@@ -57,3 +77,25 @@ export default defineComponent({
   }
 });
 </script>
+
+<style scoped>
+.pokemon-card-header {
+  background-color: #f2f2f2;
+  padding: 15px;
+  text-align: center;
+}
+
+.pokemon-card-image {
+  text-align: center;
+  padding: 20px;
+}
+
+.pokemon-card-image img {
+  max-width: 100%;
+  height: auto;
+}
+
+.pokemon-card-details {
+  padding: 15px;
+}
+</style>
